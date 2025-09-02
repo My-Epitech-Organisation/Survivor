722f421171727fdfd7a24c38209372ab48a1a537
albanrss <alban.roussee@epitech.eu>
2025-09-02 14:03:09 +0200

    feat (login/signin): the frontend of the login and signin pages are done

--- frontend/components/Navigation.tsx
+++ frontend/components/Navigation.tsx
@@ -16,8 +16,8 @@ export default function Navigation() {
 
   return (
     <nav className="bg-white shadow-sm border-b">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between">
+        <div className="flex justify-between h-16 w-full">
           <div className="flex items-center">
             <Link href="/" className="text-2xl font-bold text-blue-600">
               JEB
@@ -38,6 +38,11 @@ export default function Navigation() {
               </Link>
             ))}
           </div>
+        </div>
+        <div className="flex w-fit pl-8 items-center">
+            <Link href="/login" className="text-gray-600 hover:text-blue-600 font-bold">
+              Login
+            </Link>
         </div>
       </div>
     </nav>
