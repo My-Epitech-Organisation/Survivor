d6242083ade8b88bba8599068a16e14eaad4be4f
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-09-02 14:33:33 +0200

    feat(front): Adding a ProjectCardComponent

---
+++ frontend/app/types/project.ts
@@ -0,0 +1,11 @@
+// types/project.ts
+export interface ProjectProps {
+  ProjectId?: number;
+  ProjectName: string;
+  ProjectDescription: string;
+  ProjectFounders: string[];
+  ProjectContacts: string[];
+  ProjectProgess: string[];
+  ProjectNeeds: string[];
+  ProjectLink: string;
+}
