647ad0bbbee08d94fd1ade02678b1630dbf086f4
Nduboi <63715705+nduboi@users.noreply.github.com>
2025-09-02 15:51:17 +0200

    feat(projects): center title and enhance grid layout for project cards

--- frontend/app/projects/page.tsx
+++ frontend/app/projects/page.tsx
@@ -75,8 +75,8 @@ export default function Projects() {
 
       <main className="max-w-[90rem] mx-auto py-6 sm:px-6">
         <div className="px-4 py-6 sm:px-0">
-          <h1 className="text-3xl font-bold text-gray-900 mb-6">Projects</h1>
-            <div className='grid grid-cols-4 gap-6 p-2'>
+          <h1 className="text-3xl font-bold text-gray-900 mb-6 text-center md:text-start">Projects</h1>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 gap-6 p-2'>
               {projects.map(project => (
                 <ProjectCard key={project.ProjectId} ProjectName={project.ProjectName} ProjectDescription={project.ProjectDescription} ProjectContacts={project.ProjectContacts} ProjectFounders={project.ProjectFounders} ProjectNeeds={project.ProjectNeeds} ProjectProgess={project.ProjectProgess} ProjectLink={project.ProjectLink}/>
               ))}
