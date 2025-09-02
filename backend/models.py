e1fb8627290d68e4151dddda703e2485a41e613a
eliotttesnier <eliott.tesnier@epitech.eu>
2025-09-02 18:07:30 +0200

    refactor(backend): remove useless comment block

--- backend/models.py
+++ backend/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 
-
-# class Partner(models.Model):
-#     """
-#     Represents a partner.
-#     """
-
-#     name = models.CharField(max_length=255, null=False)
-#     legal_status = models.CharField(max_length=255, null=True)
-#     address = models.CharField(max_length=255, null=True)
-#     email = models.CharField(max_length=255, null=False)
-#     phone = models.CharField(max_length=255, null=True)
-#     created_at = models.CharField(max_length=255, null=True)
-#     description = models.TextField(null=True, blank=True)
-#     partnership_type = models.CharField(max_length=255, null=True)
-#     id = models.IntegerField(null=False, primary_key=True)
-
 #Message
 class Message(models.Model):
     """
@@ -27,4 +11,3 @@ class Message(models.Model):
     content = models.TextField(null=False)
     timestamp = models.DateTimeField(auto_now_add=True)
     id = models.IntegerField(null=False, primary_key=True)
-
