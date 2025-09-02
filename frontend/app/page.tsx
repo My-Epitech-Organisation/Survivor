a446640e79ab74dffa3b25546c0d09f10959a5e7
albanrss <alban.roussee@epitech.eu>
2025-09-02 14:52:20 +0200

    fix (home): use alias @ instead of full path to components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
-import Navigation from '../components/Navigation';
-import Footer from '../components/Footer';
+import Navigation from '@/components/Navigation';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
