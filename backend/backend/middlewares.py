6f5f914892986d1cbcbfc337ef3a618ed33b93d8
Paul-Antoine Salmon <paul-antoine.salmon@epitech.eu>
2025-09-04 15:50:33 +0200

    style(middlewares): remove unnecessary whitespace in SwaggerCSPMiddleware

--- backend/backend/middlewares.py
+++ backend/backend/middlewares.py
@@ -7,12 +7,12 @@ class SwaggerCSPMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-        
+
         # Relax the CSP for the Swagger UI and Redoc pages
         if request.path in ['/api/docs/', '/api/redoc/']:
             # Remove existing CSP headers if they exist
             for header in ['Content-Security-Policy', 'X-Content-Security-Policy']:
                 if header in response:
                     del response[header]
-        
+
         return response
