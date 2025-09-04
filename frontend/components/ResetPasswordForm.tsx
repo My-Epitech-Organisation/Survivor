315bf479a746701468ce4101ed7b35292a5957d8
albanrss <alban.roussee@epitech.eu>
2025-09-04 11:59:19 +0200

    fix (reset password): removed unused imports

--- frontend/components/ResetPasswordForm.tsx
+++ frontend/components/ResetPasswordForm.tsx
@@ -12,9 +12,7 @@ import {
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react"
-import { useRouter } from "next/navigation"
 import { getAPIUrl } from "@/lib/config"
-import { useAuth } from "@/contexts/AuthContext"
 
 export function ResetPasswordForm({
   className,
