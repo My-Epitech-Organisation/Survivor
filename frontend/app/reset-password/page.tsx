50a90d22df40d3ed852c90b28b3128ab3814c363
albanrss <alban.roussee@epitech.eu>
2025-09-04 11:16:27 +0200

    feat (ResetPassword): create reset password page with navigation and footer

---
+++ frontend/app/reset-password/page.tsx
@@ -0,0 +1,20 @@
+import Navigation from '@/components/Navigation';
+import Footer from "@/components/Footer";
+import { ResetPasswordForm } from '@/components/ResetPasswordForm';
+
+
+export default function SignUpPage() {
+  return (
+    <>
+      <div className="bg-muted h-screen flex flex-col w-full">
+        <Navigation />
+        <div className="flex-1 flex flex-col items-center justify-center gap-6 p-6 md:p-10">
+          <div className="flex w-full max-w-sm flex-col gap-6">
+            <ResetPasswordForm />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
