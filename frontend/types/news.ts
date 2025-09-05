3060656bacdb5354eb0b78e44cd429d50ff8f357
albanrss <alban.roussee@epitech.eu>
2025-09-05 14:21:40 +0200

    feat(news): add NewsItem, NewsDetailItem, and NewsCardProps interfaces

---
+++ frontend/types/news.ts
@@ -0,0 +1,17 @@
+export interface NewsItem {
+  id: number;
+  title: string;
+  category: string;
+  news_date: string;
+  location: string;
+  startup_id: number;
+}
+
+export interface NewsDetailItem extends NewsItem {
+  description: string;
+  pictureURL: string;
+}
+
+export interface NewsCardProps {
+    item: NewsItem;
+}
