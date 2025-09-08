731759745942d070389fb11cf93446b3b905150c
albanrss <alban.roussee@epitech.eu>
2025-09-08 16:30:34 +0200

    fix (lint): removed linting on specific interface

--- frontend/types/project.ts
+++ frontend/types/project.ts
@@ -42,6 +42,7 @@ export interface ProjectDetailsProps {
   ProjectWebsite: string;
 }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface ProjectDetails extends ProjectDetailsProps {}
 
 export interface ProjectProfileFormData {
