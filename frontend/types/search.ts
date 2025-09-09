b5d63a9cbc73eed5138a3e00c19ead1620c5c5a2
albanrss <alban.roussee@epitech.eu>
2025-09-09 15:28:37 +0200

    feat(search): enhance type safety by adding specific entity interfaces and updating SearchItem entity type

--- frontend/types/search.ts
+++ frontend/types/search.ts
@@ -1,10 +1,51 @@
+
+export interface NewsSearchEntity {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  location: string;
+  news_date: string;
+  result_type: string;
+}
+
+export interface EventSearchEntity {
+  id: number;
+  name: string;
+  description: string;
+  event_type: string;
+  location: string;
+  dates: string;
+  result_type: string;
+}
+
+export interface SearchProjectFounder {
+  id: number;
+  name: string;
+}
+
+export interface ProjectSearchEntity {
+  id: number;
+  name: string;
+  description?: string;
+  sector: string;
+  maturity: string;
+  address: string;
+  website_url: string;
+  project_status: string;
+  founders: SearchProjectFounder[];
+  result_type: string;
+}
+
+export type ItemEntity = NewsSearchEntity | EventSearchEntity | ProjectSearchEntity;
+
 export interface SearchItem {
   title: string;
   description: string;
   score: number;
   id: number;
   type: string;
-  entity: any;
+  entity: ItemEntity;
 }
 
 export interface SearchResponse {
